998c832f816c6307105f1750f4f127590e7d2661
chandu1920698 <chandu.muthumula@gmail.com>
2025-02-16 05:35:23 +0530

    Update Currency Convertor App

--- force-app/main/default/lwc/currencyConvertorApp/currencyConvertorApp.js
+++ force-app/main/default/lwc/currencyConvertorApp/currencyConvertorApp.js
@@ -1,22 +1,44 @@
 import { LightningElement } from 'lwc';
-import {countryCodeList} from 'c/countryCodeList';
-import currencyConvertorAssets from '@salesforce/resourceUrl/currencyConvertorAssets';
-export default class CurrencyConvertorApp extends LightningElement {
+import {countryCodeList} from 'c/countryCodeList'
+import currencyConverterAssets from '@salesforce/resourceUrl/currencyConverterAssets'
+import currecncyConvertorAccessKey from '@salesforce/label/c.Currency_Convertor_Access_Key';
+export default class CurrencyConverterApp extends LightningElement {
+    accessKey = currecncyConvertorAccessKey;
+  currencyImage = currencyConverterAssets +'/currencyConverterAssets/currency.svg'
+  countryList = countryCodeList
+  countryFrom = "USD"
+  countryTo = "AUD"
+  amount =''
+  result
+  error 
 
-    countryList = countryCodeList;
-    currencyFrom = "INR";
-    currencyTo = "USD";
-
-    currencyImage = currencyConvertorAssets + "/currencyConvertorAssets/currency.svg";
-
-    connectedCallback() {
-        console.log("this.connectedCallback");
-        console.log(this.countryList);
-    }
-    handleChange(event) {
-        const {name, value} = event.target;
-        console.log("name -> "+ name);
-        console.log("value -> "+ value);
-        this[name] = value;
+  connectedCallback() {
+    console.log("currecncyConvertorAccessKey -> " + this.accessKey);
+  }
+  handleChange(event){
+    const {name, value} = event.target
+    console.log("name", name)
+    console.log("value", value)
+    console.log("currecncyConvertorAccessKey -> " + this.accessKey);
+    this[name] = value
+  }
+  submitHandler(event){
+    event.preventDefault()
+    this.convert()
+  }
+  
+  async convert(){
+    const API_URL = `https://v6.exchangerate-api.com/v6/${this.accessKey}/pair/${this.countryFrom}/${this.countryTo}`;
+    try{
+        const data = await fetch(API_URL)
+        const jsonData = await data.json()
+        debugger;
+        // this.result = (Number(this.amount) * jsonData.result).toFixed(2)
+        this.result = (Number(this.amount) * jsonData.conversion_rate).toFixed(2)
+        console.log(this.result)
+      } catch(error){
+        console.log(error)
+        this.error="An error occurred. Please try again..."
+      }
     }
 }
