63194b44d4526f25eb32a6e1f36a74f9c9ac8197
chandu1920698 <chandu.muthumula@gmail.com>
2025-02-23 13:31:23 +0530

    Implemented Lazy Loading

--- force-app/main/default/lwc/portfolioPersonalProjects/portfolioPersonalProjects.js
+++ force-app/main/default/lwc/portfolioPersonalProjects/portfolioPersonalProjects.js
@@ -1,6 +1,5 @@
 import { LightningElement, api, wire, track } from 'lwc';
-import getProjectDetails from '@salesforce/apex/ProjectsControllerClass.getProjectDetails';
-import { getRelatedListRecords } from 'lightning/uiRelatedListApi';
+import getProjects from '@salesforce/apex/ProjectsControllerClass.getProjects';
 
 export default class PortfolioPersonalProjects extends LightningElement {
 
@@ -10,71 +9,56 @@ export default class PortfolioPersonalProjects extends LightningElement {
     @track projectDetails;
 
     connectedCallback() {
-        console.log('ppp recordId => ' + JSON.stringify(this.recordId));
+        this.showSpinner = true;
+        console.log('connectedCallback PortfolioPersonalProjects this.recordId => ' + JSON.stringify(this.recordId));
+        this.getProjectDetails();
+        
     }
 
-    @wire(getRelatedListRecords, {
-        parentRecordId : '$recordId',
-        relatedListId : 'Projects__r',
-        fields : ['Project__c.Project_Website__c',
-        'Project__c.Name',
-        'Project__c.Description__c',
-        'Project__c.CreatedDate',
-        'Project__c.Image_1__c',
-        'Project__c.Image_2__c',
-        'Project__c.Image_3__c',
-        'Project__c.Image_4__c',
-        'Project__c.Image_5__c']
-    })wireProjectDetails({data, error}) {
-        this.showSpinner = true;
-        if(data) {
-            console.log("data projectDetails -> " + JSON.stringify(data));
-            this.projectDetails = data.records.map(item => {
-                const {Name, Project_Website__c, Description__c, CreatedDate, Image_1__c, Image_2__c, Image_3__c, Image_4__c, Image_5__c} = item.fields;
+    getProjectDetails() {
+        getProjects({portfolioRecordId : this.recordId})
+        .then(data => {
+            // console.log('ppp projectDetails => ' + JSON.stringify(data));
 
-                let projectId = item.id;
-                let projectName = this.getFieldData(Name);
-                let projectDescription = this.getFieldData(Description__c);
-                let projectWebsite = this.getFieldData(Project_Website__c);
-                let createdDateTime = this.getDateTime(this.getFieldData(CreatedDate));
+            this.projectDetails = [];
+            data.forEach(project => {
+                let projectId = project.Id;
+                let projectName = project.Name;
+                let projectDescription = project.Description__c;
+                let projectWebsite = project.Project_Website__c;
                 let imageUrls = [];
-                let projectImage1 = this.getProjectImage(this.getFieldData(Image_1__c));
+                let projectImage1 = this.getProjectImage(project.Image_1__c);
                 if(projectImage1 != null) {
                     imageUrls.push(projectImage1);
                 }
-                let projectImage2 = this.getProjectImage(this.getFieldData(Image_2__c));
+                let projectImage2 = this.getProjectImage(project.Image_2__c);
                 if(projectImage2 != null) {
                     imageUrls.push(projectImage2);
                 }
-                let projectImage3 = this.getProjectImage(this.getFieldData(Image_3__c));
+                let projectImage3 = this.getProjectImage(project.Image_3__c);
                 if(projectImage3 != null) {
                     imageUrls.push(projectImage3);
                 }
-                let projectImage4 = this.getProjectImage(this.getFieldData(Image_4__c));
+                let projectImage4 = this.getProjectImage(project.Image_4__c);
                 if(projectImage4 != null) {
                     imageUrls.push(projectImage4);
                 }
-                let projectImage5 = this.getProjectImage(this.getFieldData(Image_5__c));
+                let projectImage5 = this.getProjectImage(project.Image_5__c);
                 if(projectImage5 != null) {
                     imageUrls.push(projectImage5);
                 }
-
-                return {projectId, projectName, projectDescription, projectWebsite, imageUrls, createdDateTime};
+                this.projectDetails.push({projectId, projectName, projectDescription, projectWebsite, imageUrls});
             });
 
-            this.projectDetails.sort((a, b) => new Date(a.createdDateTime) - new Date(b.createdDateTime));
-
-            this.projectDetails = this.projectDetails.reverse();
-            
             console.log("this.projectDetails -> " + JSON.stringify(this.projectDetails));
 
             if(this.projectDetails.length != 0) {
                 this.showSpinner = false;
             }
-        } else {
-            console.log("error -> " + JSON.stringify(error));
-        }
-    };
+        }).catch(error => {
+            console.log("connectedCallback PortfolioPersonalProjects this.recordId => " + JSON(error));
+        });
+    }
 
     getProjectImage(imageData) {
 
@@ -96,28 +80,5 @@ export default class PortfolioPersonalProjects extends LightningElement {
 
     getFieldData(data) {
         return data?.displayValue || data?.value;
-    }
-
-    getDateTime(dateString) {
-        // Split the string into parts
-        const [datePart, timePart] = dateString.split(", ");
-        const [day, month, year] = datePart.split("/");
-        const [time, modifier] = timePart.split(" ");
-        let [hours, minutes] = time.split(":");
-
-        // Convert 12-hour format to 24-hour format
-        if (modifier === "pm" && hours !== "12") {
-            hours = parseInt(hours, 10) + 12;
-        }
-        if (modifier === "am" && hours === "12") {
-            hours = "00"; // Midnight case
-        }
-
-        // Create the Date object
-        const dateObject = new Date(`${year}-${month}-${day}T${hours}:${minutes}:00`);
-
-        console.log(dateObject);
-
-        return dateObject;
     }
 }
